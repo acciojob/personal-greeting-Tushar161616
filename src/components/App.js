4d79eef6c021cc526e5ba985c407dcd18e12faea
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2025-08-24 03:36:29 +0000

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [name,setName] = useState("");
+  const input = document.getElementById("intext");
+
+  function change(e){
+   
+    if(input.innerText !== ""){
+    setName(e.target.value);
+    }
+  }
+ 
+ 
   return (
     <div>
-        {/* Do not remove the main div */}
+        <label for="intext">Enter your name:</label><br></br>
+        <input type="text" id="intext" onChange={change}></input>
+        <p>Hello,{name}!</p>
     </div>
   )
 }
